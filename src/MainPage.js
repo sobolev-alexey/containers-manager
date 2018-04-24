a90792406bc5ba268d8f4f193a9f2966c0e0d6db
Alexey Sobolev <>
2018-04-24 16:18:39 +0200

    Call list function to retrieve list of containers. Added notifications

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,23 +1,91 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
 import { connect } from 'react-redux';
-import { Button } from 'react-md';
+import { toast } from 'react-toastify';
+import { Button, List, ListItem, Subheader } from 'react-md';
 import isEmpty from 'lodash-es/isEmpty';
-// import upperFirst from 'lodash-es/upperFirst';
+import api from './api';
+import config from './config.json';
+import { storeContainers } from './store/containers/actions';
+import Notification from './Notification';
 import './MainPage.css';
 
 class MainPage extends Component {
+  state = {
+    showLoader: false,
+  };
+
   componentDidMount() {
-    const { auth, history } = this.props;
+    const { auth, containers, history } = this.props;
     if (isEmpty(auth)) {
       history.push('/login');
+    } else {
+      if (isEmpty(containers)) {
+        this.getContainers();
+      }
     }
   }
 
+  notifySuccess = message => toast.success(message);
+  notifyError = message => toast.error(message);
+
+  getContainers = () => {
+    const statuses = this.props.auth.previousEvent;
+
+    this.setState({ showLoader: true });
+
+    api
+      .post(`${config.rootURL}/list`, { statuses })
+      .then(response => {
+        this.setState({ showLoader: false });
+        if (response.data && response.data.length > 0) {
+          this.notifySuccess('Loaded containers');
+          this.props.storeContainers(response.data[0]);
+        }
+      })
+      .catch(error => {
+        console.log('Response error', error.response);
+        this.setState({ showLoader: false });
+        this.notifyError('Error loading containers');
+      });
+  };
+
   render() {
+    const { auth, containers, history } = this.props;
+    const { showLoader } = this.state;
     return (
       <div className="App">
-        <Button raised onClick={() => this.props.history.push('/new')}>Create new container</Button>
+        <Button raised onClick={() => history.push('/new')}>
+          Create new container
+        </Button>
+        <div className={`bouncing-loader ${showLoader ? 'visible' : ''}`}>
+          <div />
+          <div />
+          <div />
+        </div>
+        <div className={`md-block-centered ${showLoader ? 'hidden' : ''}`}>
+          <List className="md-cell md-paper md-paper--1">
+            <Subheader
+              primaryText={auth.name ? `Containers of ${auth.name}` : 'Available containers'}
+              primary
+            />
+            {containers.map(container => (
+              <ListItem
+                primaryText={container.containerId}
+                secondaryText={
+                  <div>
+                    <div>
+                      {container.departure} &rarr; {container.destination}
+                    </div>
+                    <div>{container.status}</div>
+                  </div>
+                }
+                threeLines
+              />
+            ))}
+          </List>
+        </div>
+        <Notification />
       </div>
     );
   }
@@ -25,6 +93,11 @@ class MainPage extends Component {
 
 const mapStateToProps = state => ({
   auth: state.auth,
+  containers: state.containers,
 });
 
-export default connect(mapStateToProps)(MainPage);
+const mapDispatchToProps = dispatch => ({
+  storeContainers: containers => dispatch(storeContainers(containers)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
