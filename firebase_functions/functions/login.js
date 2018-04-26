557b19e7644524d16733ba01f59610ee348c83ba
Alexey Sobolev <>
2018-04-26 16:04:37 +0200

    Added CORS support for firebase functions

--- firebase_functions/functions/login.js
+++ firebase_functions/functions/login.js
@@ -1,40 +1,43 @@
 const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 module.exports = function(req, res) {
-  if (!req.body.username || !req.body.password) {
-    return res.send({ error: 'Bad Input' });
-  }
+  return cors(req, res, () => {
+    if (!req.body.username || !req.body.password) {
+      return res.send({ error: 'Bad Input' });
+    }
 
-  let response = {};
+    let response = {};
 
-  // Retrieve user data
-  admin
-    .database()
-    .ref(`users/${req.body.username}`)
-    .once('value')
-    .then(snapshot => {
-      const val = snapshot.val();
-      if (val.password === req.body.password) {
-        delete val.password;
-        response = val;
+    // Retrieve user data
+    admin
+      .database()
+      .ref(`users/${req.body.username}`)
+      .once('value')
+      .then(snapshot => {
+        const val = snapshot.val();
+        if (val.password === req.body.password) {
+          delete val.password;
+          response = val;
 
-        // Retrieve MAM data
-        admin
-          .database()
-          .ref('mam')
-          .once('value')
-          .then(snapshot => {
-            response = Object.assign({}, response, { mam: snapshot.val() });
-            return res.send(response);
-          })
-          .catch(error => {
-            return res.status(500).send({ error: 'MAM data not found' });
-          });
-      } else {
-        return res.status(403).send({ error: 'Wrong password' });
-      }
-    })
-    .catch(error => {
-      return res.status(500).send({ error: 'User not found' });
-    });
+          // Retrieve MAM data
+          admin
+            .database()
+            .ref('mam')
+            .once('value')
+            .then(snapshot => {
+              response = Object.assign({}, response, { mam: snapshot.val() });
+              return res.send(response);
+            })
+            .catch(error => {
+              return res.status(500).send({ error: 'MAM data not found' });
+            });
+        } else {
+          return res.status(403).send({ error: 'Wrong password' });
+        }
+      })
+      .catch(error => {
+        return res.status(500).send({ error: 'User not found' });
+      });
+  });
 };
