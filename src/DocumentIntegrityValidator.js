41787a9f27880a59ed5fc5f369cd6e1e96307dbc
Alexey Sobolev <>
2018-04-27 19:34:19 +0200

    WIP: file integrity validation. Retrieve Blob with axios

--- src/DocumentIntegrityValidator.js
+++ src/DocumentIntegrityValidator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { toast } from 'react-toastify';
 import { sha256 } from 'js-sha256';
-import api from './api';
+import axios from 'axios';
 import Notification from './Notification';
 
 class DocumentIntegrityValidator extends Component {
@@ -9,24 +9,26 @@ class DocumentIntegrityValidator extends Component {
   notifyError = message => toast.error(message);
 
   validate = (path, metadata) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', path, true);
-    request.responseType = 'blob';
-    request.onload = function() {
-      const reader = new FileReader();
+    axios
+      .get(path, { responseType: 'blob' })
+      .then(response => {
+        console.log(response.headers);
 
-      reader.readAsArrayBuffer(request.response);
-      reader.onload = function(e) {
-        const arrayBuffer = reader.result;
-        const hash = sha256(arrayBuffer);
-      };
-    };
-    request.send();
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(response.data);
+        reader.onload = function() {
+          const arrayBuffer = reader.result;
+          const hash = sha256(arrayBuffer);
+          console.log(hash);
+        };
+      })
+      .catch(error => {
+        console.log(error);
+      });
   };
 
   render() {
     const { path, metadata } = this.props;
-    console.log('metadata', metadata);
 
     const validateFile = this.validate(path, metadata);
     return <div>ok</div>;
