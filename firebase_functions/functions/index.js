54180973f2c729773fae8d9675a689172f0d7420
Alexey Sobolev <sobolev@mail.de>
2018-05-04 16:10:10 +0200

    Added firebase functions to update temperature and position

--- firebase_functions/functions/index.js
+++ firebase_functions/functions/index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions');
 const serviceAccount = require('./service_account.json');
 const login = require('./login');
 const list = require('./list');
+const updatePosition = require('./updatePosition');
 const updateTemperature = require('./updateTemperature');
 
 admin.initializeApp({
@@ -12,4 +13,5 @@ admin.initializeApp({
 
 exports.login = functions.https.onRequest(login);
 exports.list = functions.https.onRequest(list);
+exports.updatePosition = functions.https.onRequest(updatePosition);
 exports.updateTemperature = functions.https.onRequest(updateTemperature);
