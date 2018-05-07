392ca9f62b94ced31309efedf213d2b48df92e29
Alexey Sobolev <sobolev@mail.de>
2018-05-07 15:34:34 +0200

    Notification styling

--- src/Notification.js
+++ src/Notification.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Notification.css';
 
 // https://fkhadra.github.io/react-toastify/
 export default () => (
   <ToastContainer
     className="toast-container"
     bodyClassName="toast-body"
-    autoClose={5000}
+    position="top-center"
+    autoClose={5000000}
     hideProgressBar
     closeOnClick
     pauseOnVisibilityChange
-    draggable
+    draggable={false}
     pauseOnHover
   />
 );
