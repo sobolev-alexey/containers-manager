0416a37cbb291b7651a5e4bb725f5f6d003aae40
Alexey Sobolev <sobolev@mail.de>
2018-05-14 19:27:28 +0200

    Converted to controlled element

--- src/DetailsPage/Tabs/index.js
+++ src/DetailsPage/Tabs/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import sizeMe from 'react-sizeme';
 import { TabsContainer, Tabs, Tab } from 'react-md';
 import last from 'lodash-es/last';
@@ -9,43 +9,52 @@ import ContainerTemperature from '../Temperature';
 import ContainerLocation from '../Location';
 import './styles.css';
 
-const ContainerTabs = ({
-  activeTabIndex,
-  container,
-  statuses,
-  containerEvents,
-  size,
-  fetchComplete,
-}) => {
-  const locations = containerEvents.filter(({ position }) => !isEmpty(position));
-  return (
-    <TabsContainer className="tabsWrapper" activeTabIndex={activeTabIndex}>
-      <Tabs tabId="container-details" mobile={size.width <= 768}>
-        <Tab label="Status">
-          <StatusList statuses={statuses} />
-        </Tab>
-        {container.documents && container.documents.length > 0 ? (
-          <Tab label="Documents">
-            <ContainerDocuments container={container} />
-          </Tab>
-        ) : null}
-        {containerEvents && last(containerEvents) && last(containerEvents).temperature ? (
-          <Tab label="Temperature">
-            <ContainerTemperature data={containerEvents} />
-          </Tab>
-        ) : null}
-        {fetchComplete && containerEvents && last(containerEvents) && locations.length > 0 ? (
-          <Tab label="Location">
-            <ContainerLocation data={locations} />
-          </Tab>
-        ) : null}
-      </Tabs>
-    </TabsContainer>
-  );
-};
+class ContainerTabs extends PureComponent {
+  state = {
+    activeTabIndex: this.props.activeTabIndex,
+  };
 
-ContainerTabs.defaultProps = {
-  activeTabIndex: 0,
-};
+  static defaultProps = {
+    activeTabIndex: 0,
+  };
+
+  onTabChange = newActiveTabIndex => {
+    this.setState({ activeTabIndex: newActiveTabIndex });
+  };
+
+  render() {
+    const { container, statuses, containerEvents, size, fetchComplete } = this.props;
+    const locations = containerEvents.filter(({ position }) => !isEmpty(position));
+
+    return (
+      <TabsContainer
+        className="tabsWrapper"
+        activeTabIndex={this.state.activeTabIndex}
+        onTabChange={this.onTabChange}
+      >
+        <Tabs tabId="container-details" mobile={size.width <= 768}>
+          <Tab label="Status">
+            <StatusList statuses={statuses} />
+          </Tab>
+          {container.documents && container.documents.length > 0 ? (
+            <Tab label="Documents">
+              <ContainerDocuments container={container} />
+            </Tab>
+          ) : null}
+          {containerEvents && last(containerEvents) && last(containerEvents).temperature ? (
+            <Tab label="Temperature">
+              <ContainerTemperature data={containerEvents} />
+            </Tab>
+          ) : null}
+          {fetchComplete && containerEvents && last(containerEvents) && locations.length > 0 ? (
+            <Tab label="Location">
+              <ContainerLocation data={locations} />
+            </Tab>
+          ) : null}
+        </Tabs>
+      </TabsContainer>
+    );
+  }
+}
 
 export default sizeMe({ monitorHeight: false })(ContainerTabs);
