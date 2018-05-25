cce5e4aa2b77945fe56a3db6cceccc2d11a7d2ea
Alexey Sobolev <sobolev@mail.de>
2018-05-25 18:44:32 +0200

    Refactored MAM functions

--- src/utils/mam.js
+++ src/utils/mam.js
@@ -1,10 +1,68 @@
 import Mam from 'mam.client.js';
 import IOTA from 'iota.lib.js';
 import { isEmpty, uniqBy, pick, find, last } from 'lodash';
-import { createNewChannel, appendToChannel } from './mamFunctions';
 import { createContainer, updateContainer } from './firebase';
+import config from '../config.json';
 
-const iota = new IOTA();
+const iota = new IOTA({ provider: config.provider });
+
+// Initialise MAM State
+let mamState = Mam.init(iota);
+
+// Publish to tangle
+const publish = async data => {
+  try {
+    // Create MAM Payload - STRING OF TRYTES
+    const trytes = iota.utils.toTrytes(JSON.stringify(data));
+    const message = Mam.create(mamState, trytes);
+
+    // Save new mamState
+    updateMamState(message.state);
+
+    // Attach the payload.
+    await Mam.attach(message.payload, message.address);
+
+    return { root: message.root, state: message.state };
+  } catch (error) {
+    console.log('MAM publish error', error);
+    return null;
+  }
+};
+
+const updateMamState = newMamState => (mamState = newMamState);
+
+const createNewChannel = async (payload, secretKey) => {
+  // Set channel mode for default state
+  const defaultMamState = Mam.changeMode(mamState, 'restricted', secretKey);
+  updateMamState(defaultMamState);
+  const mamData = await publish(payload);
+  return mamData;
+};
+
+const appendToChannel = async (payload, savedMamData, secretKey) => {
+  const mamState = {
+    subscribed: [],
+    channel: {
+      side_key: secretKey,
+      mode: 'restricted',
+      next_root: savedMamData.next,
+      security: 2,
+      start: savedMamData.start,
+      count: 1,
+      next_count: 1,
+      index: 0,
+    },
+    seed: savedMamData.seed,
+  };
+  try {
+    updateMamState(mamState);
+    const mamData = await publish(payload);
+    return mamData;
+  } catch (error) {
+    console.log('MAM append error', error);
+    return null;
+  }
+};
 
 export const fetchContainer = async (root, secretKey, storeContainerCallback, setStateCalback) => {
   const containerEvents = [];
