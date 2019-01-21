498c38252c8b42821905813db9afc10d50b3a891
Alexey Sobolev <sobolev@mail.de>
2019-01-21 17:12:20 +0100

    Updated wording

--- src/SharedComponents/MiniFooter/index.js
+++ src/SharedComponents/MiniFooter/index.js
@@ -8,7 +8,7 @@ export default ({ children }) => (
       Email: <a href="mailto:contact@iota.org">contact@iota.org</a>
     </span>
     <span>
-      <Link to="/tour">Finish demo? Learn more about IOTA Trade</Link>
+      <Link to="/tour">Finish demo? Learn more about IOTA & Trade</Link>
     </span>
     <span className="copyright">
       © 2018 <a href="https://iota.org">IOTA Foundation</a> — <a href="https://www.iota.org/research/privacy-policy">Privacy Policy</a>
