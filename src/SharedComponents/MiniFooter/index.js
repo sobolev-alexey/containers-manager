8f97d9a0276cadfba3ac1fc0f1c181741fdf7763
Alexey Sobolev <sobolev@mail.de>
2019-01-21 17:01:33 +0100

    Added tour finish link to footer

--- src/SharedComponents/MiniFooter/index.js
+++ src/SharedComponents/MiniFooter/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../assets/scss/footer.scss';
 
 export default ({ children }) => (
   <footer className="footer-mini">
-    <span className="contact">
+    <span>
       Email: <a href="mailto:contact@iota.org">contact@iota.org</a>
     </span>
     <span>
+      <Link to="/tour">Finish demo? Learn more about IOTA Trade</Link>
+    </span>
+    <span className="copyright">
       © 2018 <a href="https://iota.org">IOTA Foundation</a> — <a href="https://www.iota.org/research/privacy-policy">Privacy Policy</a>
     </span>
   </footer>
